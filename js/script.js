157aa0ad67267aa34bd5773e582f28cb0a336bba
valesquiveel <amandallmoreira@gmail.com>
2023-12-14 16:28:37 -0300

    v2
    Fim algoritmo busca Cep

--- js/script.js
+++ js/script.js
@@ -3,16 +3,30 @@
 // FETCH = pega a API toda, busca a API
 
 var cep = document.querySelector('#cep');
+var resposta = document.querySelector('#resposta');
+
+resposta.innerHTML="Digite um cep de 8 digitos;"
 cep.addEventListener("focusout", consultar);
 
 function consultar(){
-    
     let cepValue=document.querySelector("#cep").value;
-    fetch(`https://viacep.com.br/ws/${cepValue}/json/`).then((response)=>{
-        return response.json()
-    }).then((data)=>{
-        let localidade = document.querySelector("#localidade");
-        localidade.value=data.localidade;
-    });
+
+    if(cepValue.length<8 || cepValue.length>8){
+        resposta.innerHTML="Digite um CEP válido!";
+
+    }else{
+        fetch(`https://viacep.com.br/ws/${cepValue}/json/`).then((response)=>{
+            return response.json()
+        }).then((data)=>{
+            let localidade = document.querySelector("#localidade");
+            localidade.value=data.localidade;
+            resposta.innerHTML="";
+        });
+    }
 }
+
+
+
+
+    
 
